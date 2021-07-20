2cfa7ca6555739219028c314f61f870220d9e15f
러리 <doralife12@naver.com>
2021-07-20 22:09:10 +0900

    Feat: Modify item in list

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,18 +1,18 @@
-import React, { useRef, useState } from 'react'
-import CreateUser from './CreateUser';
-import UserList from './UserList'
+import React, { useRef, useState } from "react";
+import CreateUser from "./CreateUser";
+import UserList from "./UserList";
 
 function App() {
   const [inputs, setInputs] = useState({
-    username: '',
-    email: ''
+    username: "",
+    email: "",
   });
   const { username, email } = inputs;
   const onChange = (event) => {
     const { name, value } = event.target;
     setInputs({
       ...inputs,
-      [name]: value
+      [name]: value,
     });
   };
 
@@ -21,16 +21,19 @@ function App() {
       id: 1,
       username: "velopert",
       email: "public.velopert@gmail.com",
+      active: true,
     },
     {
       id: 2,
       username: "tester",
       email: "tester@example.com",
+      active: false,
     },
     {
       id: 3,
       username: "liz",
       email: "liz@example.com",
+      active: false,
     },
   ]);
 
@@ -39,21 +42,37 @@ function App() {
     const newUser = {
       id: nextId.current,
       username: username,
-      email: email
+      email: email,
+      active: false,
     };
 
     setUsers(users.concat(newUser));
 
     setInputs({
-      username: '',
-      email: ''
+      username: "",
+      email: "",
     });
     nextId.current += 1;
   };
 
   const onRemove = (id) => {
     setUsers(users.filter((user) => user.id !== id));
-  }
+  };
+
+  const onToggle = (id) => {
+    setUsers(
+      users.map((user) => {
+        if (user.id === id) {
+          return {
+            ...user,
+            active: !user.active,
+          };
+        } else {
+          return user;
+        }
+      })
+    );
+  };
 
   return (
     <>
@@ -63,7 +82,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove} />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
     </>
   );
 }
