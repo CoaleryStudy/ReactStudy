bc155348edbbc0fb483095d044801df0d43f6260
러리 <doralife12@naver.com>
2021-07-20 21:34:25 +0900

    Feat: Add item to array

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import CreateUser from './CreateUser';
 import UserList from './UserList'
 
 function App() {
-  return <UserList />
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+  const { username, email } = inputs;
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com",
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com",
+    },
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const newUser = {
+      id: nextId.current,
+      username: username,
+      email: email
+    };
+
+    setUsers(users.concat(newUser));
+
+    setInputs({
+      username: '',
+      email: ''
+    });
+    nextId.current += 1;
+  };
+
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </>
+  );
 }
 
 export default App;
