f426349c59aa3d4dcd030f7d12716c5b4062ec4b
러리 <doralife12@naver.com>
2021-08-05 23:28:27 +0900

    Feat: useContext

--- react-study/src/CreateUser.js
+++ react-study/src/CreateUser.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useRef, useCallback, useContext } from "react";
+import useInputs from "./hooks/useInputs";
+import { UserDispatch } from "./App";
 
-function CreateUser({ username, email, onChange, onCreate }) {
+function CreateUser() {
+  const dispatch = useContext(UserDispatch);
+
+  const [{username, email}, onChange, reset] = useInputs({
+    username: '',
+    email: ''
+  });
+  
+  const nextId = useRef(4);
+
+  const onCreate = useCallback(() => {
+    dispatch({
+      type: "CREATE_USER",
+      user: { id: nextId.current, username, email },
+    });
+    reset();
+    nextId.current += 1;
+  }, [username, email, reset, dispatch]);
+
   return (
     <div>
       <input
