acb9cce56a8eb4f9ccca17753f3b8b267f9e3a21
러리 <doralife12@naver.com>
2021-08-12 16:22:46 +0900

    Feat: Apply Context API

--- mashup-todolist/src/App.jsx
+++ mashup-todolist/src/App.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import './App.scss';
-import TodoCreate from './components/TodoCreate';
 
+import TodoCreate from './components/TodoCreate';
 import TodoHead from './components/TodoHead';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
+import { TodoProvider } from './TodoContext';
+
+import './App.scss';
 
 function App() {
   return (
-    <TodoTemplate>
-      <TodoHead />
-      <TodoList />
-      <TodoCreate />
-    </TodoTemplate>
+    <TodoProvider>
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
+    </TodoProvider>
   );
 }
 
