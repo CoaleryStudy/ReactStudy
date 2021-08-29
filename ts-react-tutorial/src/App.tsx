cc0d9efeb30ffe21592d8ebd2a56a5412c18dcea
러리 <doralife12@naver.com>
2021-08-29 22:11:42 +0900

    Feat: Handle input with typescript

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import Counter from './Counter';
 import Greetings from './Greetings';
+import MyForm, { FormData } from './MyForm';
 
 function App() {
   const onClick = (name: string) => {
     console.log(`${name} says Hello!`);
   };
+
+  const onSubmit = (form: FormData) => {
+    console.log(form);
+  };
+
   return (
     <>
       <Greetings name="World" onClick={onClick} />
       <Counter />
+      <MyForm onSubmit={onSubmit} />
     </>
   );
 }
