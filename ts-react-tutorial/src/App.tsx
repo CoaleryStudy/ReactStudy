e7fdee4613505ec75db0af55dbde7d3277e37a74
러리 <doralife12@naver.com>
2021-08-29 22:00:45 +0900

    Feat: Counter with typescript

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Counter from './Counter';
 import Greetings from './Greetings';
 
 function App() {
   const onClick = (name: string) => {
     console.log(`${name} says Hello!`);
   };
-  return <Greetings name="World" onClick={onClick} />;
+  return (
+    <>
+      <Greetings name="World" onClick={onClick} />
+      <Counter />
+    </>
+  );
 }
 
 export default App;
