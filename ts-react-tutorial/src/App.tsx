c4030e68255c01acd9f35a123de036fa1ce48be0
러리 <doralife12@naver.com>
2021-08-30 18:50:04 +0900

    Feat: Context API with typescript

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -1,27 +1,12 @@
 import React from 'react';
-import Counter from './Counter';
-import CounterWithReducer from './CounterWithReducer';
-import Greetings from './Greetings';
-import MyForm, { FormData } from './MyForm';
 import ReducerSample from './ReducerSample';
+import { SampleProvider } from './SampleContext';
 
 function App() {
-  const onClick = (name: string) => {
-    console.log(`${name} says Hello!`);
-  };
-
-  const onSubmit = (form: FormData) => {
-    console.log(form);
-  };
-
   return (
-    <>
-      <Greetings name="World" onClick={onClick} />
-      <Counter />
-      <CounterWithReducer />
-      <MyForm onSubmit={onSubmit} />
+    <SampleProvider>
       <ReducerSample />
-    </>
+    </SampleProvider>
   );
 }
 
