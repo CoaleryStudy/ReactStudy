240cb70d8ac0a511f5afac1e5db038ad2c06223a
러리 <doralife12@naver.com>
2021-09-03 20:17:36 +0900

    Refactor: Refactoring with redux-thunk

--- ts-react-redux-tutorial/src/modules/github/actions.ts
+++ ts-react-redux-tutorial/src/modules/github/actions.ts
@@ -1,4 +1,4 @@
-import { createAction } from 'typesafe-actions';
+import { createAsyncAction } from 'typesafe-actions';
 import { GithubProfile } from '../../api/github';
 import { AxiosError } from 'axios';
 
@@ -6,10 +6,8 @@ export const GET_USER_PROFILE = 'github/GET_USER_PROFILE';
 export const GET_USER_PROFILE_SUCCESS = 'github/GET_USER_PROFILE_SUCCESS';
 export const GET_USER_PROFILE_ERROR = 'github/GET_USER_PROFILE_ERROR';
 
-export const getUserProfileRequest = createAction(GET_USER_PROFILE)();
-export const getUserProfileSuccess = createAction(
-  GET_USER_PROFILE_SUCCESS
-)<GithubProfile>();
-export const getUserProfileError = createAction(
+export const getUserProfileAsync = createAsyncAction(
+  GET_USER_PROFILE,
+  GET_USER_PROFILE_SUCCESS,
   GET_USER_PROFILE_ERROR
-)<AxiosError>();
+)<any, GithubProfile, AxiosError>();
--- ts-react-redux-tutorial/src/modules/github/thunks.ts
+++ ts-react-redux-tutorial/src/modules/github/thunks.ts
@@ -1,24 +1,8 @@
-import { AxiosError } from 'axios';
-import { ThunkAction } from 'redux-thunk';
-import { RootState } from '..';
 import { getUserProfile } from '../../api/github';
-import {
-  getUserProfileError,
-  getUserProfileRequest,
-  getUserProfileSuccess
-} from './actions';
-import { GithubAction } from './types';
+import createAsyncThunk from '../../lib/createAsyncThunk';
+import { getUserProfileAsync } from './actions';
 
-export function getUserProfileThunk(
-  username: string
-): ThunkAction<void, RootState, null, GithubAction> {
-  return async (dispatch) => {
-    dispatch(getUserProfileRequest());
-    try {
-      const userProfile = await getUserProfile(username);
-      dispatch(getUserProfileSuccess(userProfile));
-    } catch (e) {
-      dispatch(getUserProfileError(e as AxiosError));
-    }
-  };
-}
+export const getUserProfileThunk = createAsyncThunk(
+  getUserProfileAsync,
+  getUserProfile
+);
